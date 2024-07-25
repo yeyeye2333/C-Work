515007a749d81938f764574de58837cc09f9cf32
yeyeye2333 <3229833855@qq.com>
2024-07-25 15:20:48 +0800

    尝试编写dockerfile

---
+++ mychatroom/server/build/CMakeFiles/chatserver.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chatserver.dir/chat_Server.cpp.o"
+  "CMakeFiles/chatserver.dir/chat_Server.cpp.o.d"
+  "CMakeFiles/chatserver.dir/home/yy/C-Work/mychatroom/chatroom.pb.cc.o"
+  "CMakeFiles/chatserver.dir/home/yy/C-Work/mychatroom/chatroom.pb.cc.o.d"
+  "chatserver"
+  "chatserver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chatserver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
